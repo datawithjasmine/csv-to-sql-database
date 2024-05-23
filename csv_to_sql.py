f95480577ecc25c94a4fe1b89c9be64c15f9edca
Jasmine Mitchell <jasmine@Jasmines-MacBook-Pro-2.local>
2024-05-23 18:09:23 -0500

    Added inline comments

--- csv_to_sql.py
+++ csv_to_sql.py
@@ -1,6 +1,21 @@
+# CSV-TO-SQL CONVERSION PROGRAM created by JASMINE MITCHELL
+
+#           This script takes csv files and convert them to a SQL Database and Table
+#                                      --- COMMENTS ---
+
+# 1. I IMPORTED THE FOLLOWING LIBRARIES
+#    - Pandas: to create a DataFrame from the csv file and convert it into a table.
+#    - SQLite3: to set up the SQL database from the DataFrame.
+#    - OS: to check if the csv file exists.
 import pandas as pd
 import sqlite3
 import os
+
+# 2. The IMPORT_FILE function prompts the user for the name of the csv file
+#    and checks to see if it exists. If it does, it will create a table and
+#                   then set it up as a database file.
+#    If the csv file does not exist in the current directory, then it will tell
+#                          the user to try again.
 
 
 def import_file(filename, title):
@@ -15,6 +30,9 @@ def import_file(filename, title):
         if not os.path.exists(filepath):
             print("The directory or file is invalid. Please try again.")
 
+# 3. First, the program starts off with a welcome message. Then prompts the user
+#      for a name to give the file. The user also has to give the table a name.
+#     The function will then execute, making the file and the program will end.
 
 print("Welcome! This program will convert a csv file into a table.")
 

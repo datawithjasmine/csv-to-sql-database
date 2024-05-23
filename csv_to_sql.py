c963b1bd5ca0ab3f5c2ad4a2ebcc7142b614ab08
Jasmine Mitchell <jasmine@Jasmines-MacBook-Pro-2.local>
2024-05-22 22:17:07 -0500

    improved functionality

--- csv_to_sql.py
+++ csv_to_sql.py
@@ -9,17 +9,17 @@ def import_file(filename, title):
         df = pd.read_csv(filepath)
         conn = sqlite3.connect(filename)
         df.to_sql(title, conn, if_exists='replace', index=False)
-        print("Your table was successfully created.")
+        print("Your table was successfully created!")
         conn.close()
     else:
         if not os.path.exists(filepath):
             print("The directory or file is invalid. Please try again.")
 
 
-print("Welcome! This program will convert a csv file into a SQL database.")
+print("Welcome! This program will convert a csv file into a table.")
 
 database_name = input("What do you want to name this file as? (MUST END IN '.db'): ")
-database_title = input("What is the title of your database?: ")
+database_title = input("What is the title of your table? (NO SPACES! USE UNDERSCORES): ")
 
 import_file(database_name, database_title)
 
